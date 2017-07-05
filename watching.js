5d02af0ea752ab869f7848a16bb5b30f0d9d138c
Shu Kumata <shukuma11.sns@gmail.com>
2017-07-06 01:05:48 +0900

    We can unwatch

--- watching.js
+++ watching.js
@@ -1,10 +1,10 @@
 var dispatcher = new Flux.Dispatcher();
+var token = 'token ';
 
 var GetWatchingAction = {
     getWatching: function() {
         var request = new XMLHttpRequest();
         var url = "https://api.github.com/user/subscriptions";
-        var token = 'token ';
         request.open('GET', url, true);
             request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
             request.setRequestHeader('Authorization',token);
@@ -22,6 +22,25 @@ var GetWatchingAction = {
             };
             request.send();
     },
+}
+
+var unwatchAction = {
+    unwatch: function(selectedJson) {
+        var request = new XMLHttpRequest();
+        var fullName = selectedJson.full_name;
+        console.log(fullName);
+        var url = "https://api.github.com/repos/" + fullName + "/" + "subscription";
+        request.open('DELETE', url, true);
+            request.setRequestHeader('Authorization',token);
+            request.onreadystatechange = function(){
+                if (request.readyState === 4 && request.status === 204){
+                    dispatcher.dispatch({
+                        actionType: "unwatch"
+                    });
+                }
+            };
+            request.send();
+    }
 }
 
 var _watching = {data:null};
@@ -45,6 +64,8 @@ var StoredJsonStore = Object.assign({}, EventEmitter.prototype, {
             _watching.data = payload.data;
             console.log(_watching);
             StoredJsonStore.emitGetWatching();
+        } else if (payload.actionType === "unwatch") {
+            GetWatchingAction.getWatching();
         }
     })
 });
@@ -52,6 +73,11 @@ var StoredJsonStore = Object.assign({}, EventEmitter.prototype, {
 class WatchingList extends React.Component {
     constructor() {
         super();
+        this.unwatch = this.unwatch.bind(this);
+    }
+    unwatch(value) {
+        console.log(value);
+        unwatchAction.unwatch(value);
     }
     render() {
         return (
@@ -66,13 +92,9 @@ class WatchingRepositoryArea extends React.Component {
     constructor() {
         super();
         this.state = {storedData:[]};
-        this.unwatch = this.unwatch.bind(this);
         GetWatchingAction.getWatching();
     }
-    unwatch(value) {
-        // console.log(value.name);
-        // RegisterJsonAction.registerJson(value);
-    }
+
     // コンポーネントが描画されたら実行
     componentDidMount() {
         var self = this;
@@ -90,6 +112,7 @@ class WatchingRepositoryArea extends React.Component {
         if (this.state.storedData.length !== 0) {
             var storedData = this.state.storedData;
             for (var i=0;i<storedData.length;i++) {
+                console.log(storedData[i]);
                 rows.push(<WatchingList value={storedData[i]} key={storedData[i].id}></WatchingList>);
             }
         }
